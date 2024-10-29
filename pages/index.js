e879564cfcaa326a1a4677a147de0ba25518dbcb
dotlaksh <dotlaksh@outlook.com>
2024-10-29 20:04:01 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -336,7 +336,7 @@ const StockChart = () => {
       </div>
 
       {/* Chart Area */}
-      <main className="flex-grow flex items-center justify-center p-1">
+      <main className="flex-grow flex items-center justify-center">
         {loading ? (
           <div className="flex items-center justify-center">
             <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-blue-600"></div>
