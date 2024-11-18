ff2cc45bfbd67a5da30c19e3257e898db8ce3ed8
dotlaksh <dotlaksh@outlook.com>
2024-11-18 21:25:39 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,21 @@
-// app/layout.tsx
+import { ThemeProvider } from "@/components/theme-provider"
 
-import { ThemeProvider } from 'next-themes'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import "../styles/globals.css"; 
-
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
-  title: 'DotChart',
-  description: 'Stock chart application',
-}
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className} bg-background text-foreground`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+    <>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
   )
 }
