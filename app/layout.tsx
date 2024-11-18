8330de49f84cfba7b21ec78edf7b2b410699e6f1
dotlaksh <dotlaksh@outlook.com>
2024-11-18 21:32:58 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,20 @@
-import { ThemeProvider } from "@/src/components/theme-provider"
+// app/layout.tsx
+
+import { ThemeProvider } from 'next-themes'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import "../styles/globals.css"; 
+import { ThemeProvider } from "../src/components/theme-provider"
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'DotChart',
+  description: 'Stock chart application',
+}
+
+
+
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
