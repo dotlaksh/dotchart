b7d593a002300e3c03dabd852b8563f307bf2db9
dotlaksh <dotlaksh@outlook.com>
2024-11-18 20:24:07 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -117,7 +117,7 @@ export default function StockChart() {
   const { theme, setTheme } = useTheme();
 
   const getChartHeight = useCallback(() => {
-    return window.innerWidth < 640 ? 700 : window.innerWidth < 1024 ? 320 : 800;
+    return window.innerWidth < 640 ? 640 : window.innerWidth < 1024 ? 320 : 800;
   }, []);
 
   useEffect(() => {
