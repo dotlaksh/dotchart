c2d7b7a063bc106d24e3339b28b6b50675a4690d
dotlaksh <dotlaksh@outlook.com>
2024-11-18 21:52:43 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,15 @@
 // app/layout.tsx
+import { ThemeProvider } from 'next-themes';
+import './globals.css';
 
-import { ThemeProvider } from "../src/components/theme-provider"
-
-import "../styles/globals.css"; 
-
-export default function RootLayout({ children }: RootLayoutProps) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <html lang="en" suppressHydrationWarning>
-        <head />
-        <body>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
-        </body>
-      </html>
-    </>
-  )
+    <html lang="en">
+      <body>
+        <ThemeProvider attribute="class" defaultTheme="system">
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
