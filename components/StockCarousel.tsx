f25621716f451598362fc9f7ae982c64a75ac08d
dotlaksh <dotlaksh@outlook.com>
2024-11-27 11:49:39 +0530

    Update StockCarousel.tsx

--- components/StockCarousel.tsx
+++ components/StockCarousel.tsx
@@ -96,7 +96,6 @@ const StockCarousel: React.FC = () => {
             <SelectValue placeholder="Select range" />
           </SelectTrigger>
           <SelectContent>
-            <SelectItem value="6mo">6 Months</SelectItem>
             <SelectItem value="1y">1 Year</SelectItem>
             <SelectItem value="5y">5 Years</SelectItem>
             <SelectItem value="max">Max</SelectItem>
