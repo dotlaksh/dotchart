973429d395a8d50b36777e67fad17d07737480cc
dotlaksh <dotlaksh@outlook.com>
2024-11-27 21:43:19 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { ThemeToggle } from '@/components/theme-toggle'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col items-center justify-between p-2">
       <div className="z-10 w-full max-w-6xl items-center justify-between font-sans text-sm">
         <div className="flex justify-between items-center mb-4">
           <h1 className="text-2xl font-bold">dotChart</h1>
