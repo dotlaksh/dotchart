e4c72140a599e61520d3c858e0e1873832a0c552
dotlaksh <dotlaksh@outlook.com>
2024-11-27 21:24:29 +0530

    Update StockChart.tsx

--- components/StockChart.tsx
+++ components/StockChart.tsx
@@ -126,7 +126,7 @@ const StockChart: React.FC<StockChartProps> = ({ symbol, interval, range }) => {
       ) : (
         <>
           <div className="mt-4 ">
-            <h3 className="text-lg font-semibold">{symbol}</h3>
+            <h3 className="text-md font-semibold">{symbol}</h3>
             {todayPrice !== null && priceChange !== null && (
               <div className="flex items-center text-sm mt-1">
                 <span className="font-medium mr-2">{todayPrice.toFixed(2)}</span>
