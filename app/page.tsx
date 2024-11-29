734b48ae7b0c091e2f6a53f71b46ed3c57cd8b4a
dotlaksh <dotlaksh@outlook.com>
2024-11-29 11:43:04 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -57,8 +57,8 @@ export default function Home() {
 
   return (
     <main className="flex flex-col h-screen" ref={pageRef}>
-      <div className="flex justify-between items-center p-4 bg-background relative z-50">
-        <h1 className="text-2xl font-bold">dotChart</h1>
+      <div className="flex justify-between items-center bg-background relative z-50">
+        <h1 className="text-lg font-bold">dotChart</h1>
         <div className="flex items-center gap-2">
           <Select
             value={currentCategoryIndex.toString()}
