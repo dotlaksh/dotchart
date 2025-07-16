a16119de24c192e28027be87b29013d761fa340b
dotlaksh <dotlaksh@outlook.com>
2025-07-16 10:21:38 +0530

    Update stockList.ts

--- lib/stockList.ts
+++ lib/stockList.ts
@@ -1,6 +1,4 @@
 
-import largecapData from '../public/largecap.json';
-import midcapData from '../public/midcap.json';
 import smallcapData from '../public/smallcap.json';
 import microcapData from '../public/microcap.json';
 import othersData from '../public/others.json';
@@ -9,8 +7,6 @@ import othersData from '../public/others.json';
 
 
 export const stockCategories = [
-  { name: 'LargeCaps', data: largecapData },
-  { name: 'MidCaps', data: midcapData },
   { name: 'SmallCaps', data: smallcapData },
   { name: 'MicroCaps', data: microcapData },
   { name: 'Others', data: othersData }
