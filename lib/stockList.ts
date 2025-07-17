648a967b2092ba9a9eb28c72e6943afecdc8ec24
dotlaksh <dotlaksh@outlook.com>
2025-07-17 11:39:43 +0530

    Update stockList.ts

--- lib/stockList.ts
+++ lib/stockList.ts
@@ -1,13 +1,17 @@
 
 import smallcapData from '../public/smallcap.json';
 import microcapData from '../public/microcap.json';
+import ipoData from '../public/ipo.json';
+
 
 
 
 
 export const stockCategories = [
   { name: 'SmallCaps', data: smallcapData },
-  { name: 'MicroCaps', data: microcapData }
+  { name: 'MicroCaps', data: microcapData },
+  { name: 'IPOs', data: ipoData }
+
 
 ];
 
