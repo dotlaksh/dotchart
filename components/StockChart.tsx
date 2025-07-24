6565196c0a39bc33907e0c9ebcb105812c81e7ec
dotlaksh <dotlaksh@outlook.com>
2025-07-24 11:01:59 +0530

    Update StockChart.tsx

--- components/StockChart.tsx
+++ components/StockChart.tsx
@@ -11,7 +11,7 @@ import clsx from "clsx"
 
 // INTERVAL BUTTONS
 const intervals: { label: string; value: string; range: string }[] = [
-  { label: 'D', value: '1d', range: '3mo' },
+  { label: 'D', value: '1d', range: '1y' },
   { label: 'W', value: '1wk', range: '3y' },
   { label: 'M', value: '1mo', range: '10y' }
   ];
