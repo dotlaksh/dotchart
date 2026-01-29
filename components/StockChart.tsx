859c3c3a5532cb573f8fb52db0aa317b9d490656
dotlaksh <dotlaksh@outlook.com>
2026-01-29 11:57:05 +0530

    Increase height of StockChart on medium screens

--- components/StockChart.tsx
+++ components/StockChart.tsx
@@ -292,7 +292,7 @@ const StockCarousel: React.FC<StockCarouselProps> = ({
           <CardContent className="p-0">
             <div className="flex flex-col">
               {/* Chart Area */}
-              <div className="h-[900px] sm:h-[600px] md:h-[650px]">
+              <div className="h-[900px] sm:h-[600px] md:h-[700px]">
                 <StockChart symbol={currentStock.Symbol} interval={stockInterval} range={stockRange} />
               </div>
               
